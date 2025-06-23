306a1b877d7ffb4e4350dc7ff7a9bf0ade53812e
Mfajar24 <fajarmoh05@gmail.com>
2025-06-24 00:38:00 +0700

    Perbarui konfigurasi ke MySQL dan setup database

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
-require('dotenv').config(); // ← Tambahkan ini untuk baca .env
+require('dotenv').config(); // Untuk membaca file .env
 const { Sequelize } = require('sequelize');
 
+// Inisialisasi koneksi ke database MySQL
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
-  dialect: 'postgres',
-  logging: false,
+  dialect: 'mysql', 
+  logging: false,   
 });
 
 module.exports = db;
