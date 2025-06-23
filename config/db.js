03ad0cf9102bd8158531016c0d8c4e608f2b889c
Mfajar24 <fajarmoh05@gmail.com>
2025-06-24 01:21:14 +0700

    fix api/db.js

--- config/db.js
+++ config/db.js
@@ -1,11 +1,23 @@
-require('dotenv').config(); // Untuk membaca file .env
+require('dotenv').config();
+const fs = require('fs');
 const { Sequelize } = require('sequelize');
 
-// Inisialisasi koneksi ke database MySQL
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  dialect: 'mysql', 
-  logging: false,   
-});
+const db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql', // atau 'postgres'
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        ca: fs.readFileSync(__dirname + '/../certs/ca.pem').toString(), // arahkan ke file cert
+        rejectUnauthorized: true,
+      },
+    },
+  }
+);
 
 module.exports = db;
